83d5cf9f8330079e8edb81752ea9be6ce184d5c5
geezerP <mwaurameshack64@gmail.com>
2020-04-21 11:24:01 +0300

    add movies template

--- app/views.py
+++ app/views.py
@@ -9,4 +9,12 @@ def index():
     View root page function that returns the index page and its data
 
     '''
-    return render_template('index.html')
+    
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    #View Movie page function that returns the movie details page and its data
+    title = 'Jinja templating'
+    return render_template('movie.html', id = movie_id, title = title)
