6220f915846502e8e11b64f1a370aba8d7639d2c
geezerP <mwaurameshack64@gmail.com>
2020-04-25 16:35:13 +0300

    ch:(add test features)

--- app/requests.py
+++ app/requests.py
@@ -1,14 +1,18 @@
-from app import app
+from .main import main
 import urllib.request,json
-from .models import movie
+from .models import Movie
 
 # Getting api key
-api_key = app.config['MOVIE_API_KEY']
-
+api_key = None
 # Getting the movie base url
-base_url = app.config["MOVIE_API_BASE_URL"]
+base_url = None
 
-Movie = movie.Movie
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['MOVIE_API_KEY']
+    base_url = app.config['MOVIE_API_BASE_URL']
+
+Movie = Movie
 
 def get_movies(category):
     '''
